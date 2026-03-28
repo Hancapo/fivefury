eeef7b111e63ba16f2f48af4bb1bce976b6be790
MrVicho13 <vicho0202@gmail.com>
2026-03-28 05:49:55 -0300

    Ignore local examples directory

--- examples/check_state.py
+++
@@ -1,8 +0,0 @@
-from fivefury import Archetype, Entity, GameFileCache, Ymap, Ytyp, create_rpf  # noqa: E402,F401
-
-from pathlib import Path
-
-
-GAME_PATH: Path = Path(r"C:\Program Files (x86)\Steam\steamapps\common\Grand Theft Auto V")
-gfc = GameFileCache(GAME_PATH, dlc_level="mp2025_02_g9ec", verbose=True)
-gfc.scan(use_index_cache=False)
